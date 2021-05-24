2b0509343a63323935abbfbba42b2fd65fb9b806
Nikolay Tsenkov <nikolay@tsenkov.net>
2021-05-24 10:56:11 +0300

    Improve: Learning from users: Run multiple typed ads from the same space.

--- src/app/ui/module/typed-ad/controller/typed-ad.js
+++ src/app/ui/module/typed-ad/controller/typed-ad.js
@@ -3,18 +3,76 @@
 
 var Typed = require( "typedjs" );
 
+var ADS = [
+
+	{
+		url: "https://www.anrdoezrs.net/click-100274574-13899542",
+		text: [
+			"Hey there!",
+			"Wanna record a full song?",
+			"Right in your browser?",
+			"Click right... HERE!"
+		],
+		weight: 3
+	},
+
+	{
+		url: "https://forms.gle/E4ctbsMXk1n2U8dM9",
+		text: [
+			"Hey there!",
+			"This is NV-1's creator, Nikolay - nice to meet you!",
+			"Would you be interested in taking a course on Audio Processing & Synthesis in the Browser?",
+			"CLICK HERE to let me know!"
+		],
+		weight: 1
+	},
+
+	{
+		url: "https://forms.gle/7rr44vAqBrAKxUEU7",
+		text: [
+			"Hey there!",
+			"Do you want to build your own synth?",
+			"Click HERE to make an inquiry!"
+		],
+		weight: 1
+	},
+
+	{
+		url: "https://forms.gle/Tnn2xfNtzjfPzGUM8",
+		text: [
+			"Hey there!",
+			"Do you teach with NV-1?",
+			"CLICK HERE to help me better understand how you use my synth!"
+		],
+		weight: 1
+	}
+];
+
+function getAd () {
+
+	var indexChance = [];
+
+	ADS.forEach((value, index) => {
+
+		indexChance = indexChance.concat ( Array.from( { length: Math.floor( value.weight * 10 ) } ).map( x => index ));
+	});
+
+	return ADS [
+		indexChance[ Math.floor( Math.random() * indexChance.length ) ]
+	];
+}
 
 module.exports = function( mod ) {
 
 	mod.controller( "TypedAdController", [ "$scope", function ($scope) {
 
+		var self = this,
+			ad = getAd ();
+
+		self.url = ad.url;
+
 		$scope.options = {
-			strings: [
-				"Hey there!",
-				"Wanna record a full song?",
-				"Right in your browser?",
-				"Click right... HERE!"
-			],
+			strings: ad.text,
 			typeSpeed: 80,
 			startDelay: 1200,
 			smartBackspace: true,
