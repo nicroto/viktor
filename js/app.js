604d48704ea3107a3c170d5acefd01c8c6dda361
Nikolay Tsenkov <nikolay@tsenkov.net>
2021-05-24 10:56:40 +0300

    update website

--- js/app.js
+++ js/app.js
@@ -52891,8 +52891,7 @@ var angular = require( "angular" ),
 		require( "./module/patch-library/module" ).name,
 		require( "./module/instruments/synth/module" ).name,
 		require( "./module/maillist/module" ).name,
-		// require( "./module/typed-ad/module" ).name,
-		require( "./module/typed-ad-custom-build/module" ).name,
+		require( "./module/typed-ad/module" ).name,
 		require( "./module/footer/module" ).name,
 		require( "./module/t-shirts/module" ).name,
 
@@ -52954,7 +52953,7 @@ require( "./controller/modulation-wheel" )( mod );
 require( "./controller/keyboard" )( mod );
 
 module.exports = mod;
-},{"./controller/compressor":41,"./controller/delay":42,"./controller/keyboard":43,"./controller/master-controls":44,"./controller/master-volume":45,"./controller/modulation-wheel":46,"./controller/pitch-bend":47,"./controller/reverb":48,"./module/footer/module":52,"./module/instruments/synth/module":62,"./module/maillist/module":73,"./module/patch-library/module":77,"./module/settings/module":81,"./module/t-shirts/module":84,"./module/typed-ad-custom-build/module":87,"./template/compressor.html":89,"./template/daw.html":90,"./template/delay.html":91,"./template/keyboard.html":92,"./template/master-controls.html":93,"./template/master-volume.html":94,"./template/modulation-wheel.html":95,"./template/pitch-bend.html":96,"./template/reverb.html":97,"angular":34,"viktor-nv1-engine":20}],51:[function(require,module,exports){
+},{"./controller/compressor":41,"./controller/delay":42,"./controller/keyboard":43,"./controller/master-controls":44,"./controller/master-volume":45,"./controller/modulation-wheel":46,"./controller/pitch-bend":47,"./controller/reverb":48,"./module/footer/module":52,"./module/instruments/synth/module":62,"./module/maillist/module":73,"./module/patch-library/module":77,"./module/settings/module":81,"./module/t-shirts/module":84,"./module/typed-ad/module":87,"./template/compressor.html":89,"./template/daw.html":90,"./template/delay.html":91,"./template/keyboard.html":92,"./template/master-controls.html":93,"./template/master-volume.html":94,"./template/modulation-wheel.html":95,"./template/pitch-bend.html":96,"./template/reverb.html":97,"angular":34,"viktor-nv1-engine":20}],51:[function(require,module,exports){
 'use strict';
 
 module.exports = function( mod ) {
@@ -61389,17 +61388,76 @@ module.exports = ngModule;
 
 var Typed = require( "typedjs" );
 
+var ADS = [
+
+	{
+		url: "https://www.anrdoezrs.net/click-100274574-13899542",
+		text: [
+			"Hey there!",
+			"Wanna record a full song?",
+			"Right in your browser?",
+			"Click right... HERE!"
+		],
+		weight: 3
+	},
+
+	{
+		url: "https://forms.gle/E4ctbsMXk1n2U8dM9",
+		text: [
+			"Hey there!",
+			"This is NV-1's creator, Nikolay - nice to meet you!",
+			"Would you be interested in taking a course on Audio Processing & Synthesis in the Browser?",
+			"CLICK HERE to let me know!"
+		],
+		weight: 1
+	},
+
+	{
+		url: "https://forms.gle/7rr44vAqBrAKxUEU7",
+		text: [
+			"Hey there!",
+			"Do you want to build your own synth?",
+			"Click HERE to make an inquiry!"
+		],
+		weight: 1
+	},
+
+	{
+		url: "https://forms.gle/Tnn2xfNtzjfPzGUM8",
+		text: [
+			"Hey there!",
+			"Do you teach with NV-1?",
+			"CLICK HERE to help me better understand how you use my synth!"
+		],
+		weight: 1
+	}
+];
+
+function getAd () {
+
+	var indexChance = [];
+
+	ADS.forEach((value, index) => {
+
+		indexChance = indexChance.concat ( Array.from( { length: Math.floor( value.weight * 10 ) } ).map( x => index ));
+	});
+
+	return ADS [
+		indexChance[ Math.floor( Math.random() * indexChance.length ) ]
+	];
+}
 
 module.exports = function( mod ) {
 
 	mod.controller( "TypedAdController", [ "$scope", function ($scope) {
 
+		var self = this,
+			ad = getAd ();
+
+		self.url = ad.url;
+
 		$scope.options = {
-			strings: [
-				"Hey there!",
-				"Do you want to build your own synth?",
-				"Click HERE to make an inquiry!"
-			],
+			strings: ad.text,
 			typeSpeed: 80,
 			startDelay: 1200,
 			smartBackspace: true,
@@ -61440,11 +61498,12 @@ ngModule.run(['$templateCache', function($templateCache) {
   $templateCache.put('typed-ad.html',
     '<div ng-controller="TypedAdController as ad" class="typed-ad-container container-fluid text-center">\n' +
     '    <div class="row text-center">\n' +
-    '        <a href="https://forms.gle/7rr44vAqBrAKxUEU7" target="_blank">\n' +
+    '        <a href="{{ad.url}}" target="_blank">\n' +
     '            <div>\n' +
     '                <h3 class="type-in-here"></h3>\n' +
     '            </div>\n' +
     '        </a>\n' +
+    '        <img src="https://www.awltovhc.com/image-100274574-13899542" width="1" height="1" border="0"/>\n' +
     '    </div>\n' +
     '</div>');
 }]);
